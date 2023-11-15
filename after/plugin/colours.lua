ef128c37472d1218fa28566e1cd9a38af0811d81
Ryan Cushen <cushenr@gmail.com>
2023-11-15 18:50:48 +1100

    Make it transparent

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -1,1 +1,10 @@
+require("tokyonight").setup({
+    style = "storm",
+    transparent = true,
+    styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+    }
+})
+
 vim.cmd.colorscheme "tokyonight"
